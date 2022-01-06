1ec19ea855a211f908a5e49883106d75dc43442d
andrey56097 <batsandrey@gmail.com>
2022-01-06 09:29:34 +0200

    init commit

---
+++ src/main/java/com/example/ulbitspring/UlbitspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ulbitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlbitspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlbitspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ulbitspring/UlbitspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ulbitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlbitspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
